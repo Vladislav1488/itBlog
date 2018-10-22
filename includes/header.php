a3cdbe720bbaa25fb9828daf5868f953e5fac1d5
Владислав Мороз <vladislav.moroz103@gmail.com>
2018-10-22 11:25:38 +0300

    itBlog

---
+++ includes/header.php
@@ -0,0 +1,19 @@
+<header>
+    <nav class="container">
+        <a class="logo" href="">
+        <span>B</span>
+        <span>L</span>
+        <span>O</span>
+        <span>G</span>
+      </a>
+        <div class="nav-toggle"></div>
+        <form action="" method="get" id="searchform">
+            <button type="submit"><i>Sign</i></button>
+        </form>
+        <ul id="menu">
+            <li><a href="/">Главная</a></li>
+            <li><a href="">Портфолио</a></li>
+            <li><a href="">Об авторе</a></li>
+        </ul>
+    </nav>
+</header>
---
+++ includes/sidebar.php
@@ -0,0 +1,40 @@
+<aside>
+            <div class="widget">
+                <h3 class="widget-title">Категории</h3>
+                <ul class="widget-category-list">
+                    <li><a href="">Дизайн</a> (2)</li>
+                    <li><a href="">Вёрстка</a> (5)</li>
+                    <li><a href="">Видео</a> (1)</li>
+                </ul>
+            </div>
+            <div class="widget">
+                <h3 class="widget-title">Последние записи</h3>
+                <ul class="widget-posts-list">
+                    <li>
+                        <div class="post-image-small">
+                            <a href=""><img src="../images/beer.png"></a>
+                        </div>
+                        <h4 class="widget-post-title">Бухло</h4>
+                    </li>
+                    <li>
+                        <div class="post-image-small">
+                            <a href=""><img src="../images/article pic.png"></a>
+                        </div>
+                        <h4 class="widget-post-title">Четкий код</h4>
+                    </li>
+                    <li>
+                        <div class="post-image-small">
+                            <a href=""><img src="../images/hardbass.jpg"></a>
+                        </div>
+                        <h4 class="widget-post-title">Хардбасс</h4>
+                    </li>
+                </ul>
+            </div>
+            <div class="widget">
+                <h3 class="widget-title">Подписка на рассылку</h3>
+                <form action="" method="post" id="subscribe">
+                    <input type="email" name="email" placeholder="Ваш email" required>
+                    <button type="submit"><i class="fa fa-paper-plane-o"></i></button>
+                </form>
+            </div>
+        </aside>
